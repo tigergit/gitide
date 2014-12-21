e3e62df6274672582a353db722f4a565257727dc
Alex Zhang <okey365@outlook.com>
2014-12-21 22:35:20 +0800

    The first class file.

---
+++ src/com/fd/demos/git/gitide/GitIDE.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.fd.demos.git.gitide;
+
+/**
+ * @author Qi
+ *
+ */
+public class GitIDE {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, Git + Eclipse");
+
+	}
+
+}
